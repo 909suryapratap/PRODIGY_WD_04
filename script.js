0280d27719218c03a97ff8fbcbdf66eb1d295868
Surya Pratap Singh Chouhan <909suryapratap@gmail.com>
2025-11-22 17:29:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+async function getWeather() {
+    const location = document.getElementById('location').value;
+    const apiKey = '791c4e65ea7197f580f0c6ee098cc12f';
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`);
+    
+    const data = await response.json();
+    
+    if (data.cod === 200) {
+        document.getElementById('weather-result').innerHTML = `
+            <p><strong>${data.name}</strong>: ${data.weather[0].description}, ${data.main.temp}°C</p>`;
+        
+        const hour = new Date().getHours();
+        const isDayTime = hour > 6 && hour < 18;
+        
+        if (isDayTime) {
+            document.getElementById('day-sound').play();
+        } else {
+            document.getElementById('night-sound').play();
+        }
+    } else {
+        document.getElementById('weather-result').innerHTML = '<p>Location not found.</p>';
+    }
+}
